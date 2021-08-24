872ff1979a487a518a3034f51159700578af752e
parthpatel <parth.patel@mindinventory.com>
2021-08-24 11:25:36 +0530

    - Project basic firebase continue

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+};
+
+const Firebase = () => {
+        useEffect(() => {
+            firebase.app.initializeApp(firebaseConfig);
+        }, [])
+}
+
+export const db = firebase.database();
+
+export default Firebase;
